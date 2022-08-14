c668ab1946b89886306b0407ccc2c9a3cc89d330
Tiara <tiaranrhkmh@gmail.com>
2022-08-15 05:44:29 +0700

    Create say helllo

---
+++ SayHello.go
@@ -0,0 +1,11 @@
+package gosayhello
+
+import "fmt"
+
+func SayHelloToEveryone() {
+	fmt.Println("Hello Everyone")
+}
+
+func SayHelloToSomeone(name string) {
+	fmt.Println("Hello", name)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tiaranrhkmh/go-say-hello
+
+go 1.18
